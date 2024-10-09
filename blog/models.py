e8491d3c5664f5ba78ad86bac7461a48b2ef34bd
Jyeongjae <lovej2078@gmail.com>
2024-10-09 17:10:31 +0900

    create category_page

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,13 @@ import os
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
     class Meta:
         verbose_name_plural = 'categories'
 
