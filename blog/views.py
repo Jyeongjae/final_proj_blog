909e71737f1e0b45f154596fc65464092435faa3
Jyeongjae <lovej2078@gmail.com>
2024-11-05 14:06:38 +0900

    adjust tag

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from django.core.exceptions import PermissionDenied
 from django.db.models import Q
 from .forms import CommentForm
 from django.utils import timezone
+from django.utils.text import slugify
 
 
 
@@ -142,37 +143,6 @@ def tag_page(request, slug):
         }
     )
 
-# class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
-#     model = Post
-#     fields = ['title', 'content', 'head_image']
-#     template_name = 'blog/create_new_post.html'
-#
-#     def test_func(self):
-#         return self.request.user.is_superuser or self.request.user.is_staff
-#
-#     def form_valid(self, form):
-#         current_user = self.request.user
-#         if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
-#             form.instance.author = current_user
-#             response = super(PostCreate, self).form_valid(form)
-#
-#             tags_str = self.request.POST.get('tags_str')
-#             if tags_str:
-#                 tags_str = tags_str.strip()
-#                 tags_str = tags_str.replace(',', ';')
-#                 tags_list = tags_str.split(';')
-#                 for t in tags_list:
-#                     t = t.strip()
-#                     tag, is_tag_created = Tag.objects.get_or_create(name=t)
-#                     if is_tag_created:
-#                         tag.slug = slugify(t, allow_unicode=True)
-#                         tag.save()
-#                     self.object.tags.add(tag)
-#
-#             return response
-#
-#         else:
-#             return redirect('/blog/')
 
 def create_post(request):
     if request.method == "POST":
@@ -189,16 +159,66 @@ def create_post(request):
             author=request.user,
         )
 
-        # 태그 저장
         if tags_str:
-            tags_list = tags_str.replace(',', ';').split(';')
-            for tag_name in tags_list:
-                tag, _ = Tag.objects.get_or_create(name=tag_name.strip())
-                post.tags.add(tag)
+            tags_str = tags_str.strip().replace(',', ';')
+            tags_list = tags_str.split(';')
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
 
         return redirect(post.get_absolute_url())  # 게시물 상세 페이지로 리디렉션
 
     return render(request, "blog/create_new_post.html")
 
+
+from django.shortcuts import render, redirect
+from django.utils.text import slugify
+from .models import Post, Tag
+
+
+def unique_slug_generator(tag_name):
+    slug = slugify(tag_name, allow_unicode=True)
+    original_slug = slug
+    count = 1
+    while Tag.objects.filter(slug=slug).exists():
+        slug = f"{original_slug}-{count}"
+        count += 1
+    return slug
+
+
+def create_post(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        head_image = request.FILES.get("head_image")
+        tags_str = request.POST.get("tags_str", "")
+
+        # 새 Post 인스턴스 생성
+        post = Post.objects.create(
+            title=title,
+            content=content,
+            head_image=head_image,
+            author=request.user,
+        )
+
+        if tags_str:
+            tags_str = tags_str.strip().replace(',', ';')
+            tags_list = tags_str.split(';')
+            for t in tags_list:
+                t = t.strip()
+
+                # 새로운 Tag 인스턴스를 항상 생성하고 고유한 slug 지정
+                slug = unique_slug_generator(t)
+                tag = Tag(name=t, slug=slug)
+                tag.save()  # 고유 slug를 지정한 뒤 저장
+                post.tags.add(tag)  # 태그를 게시물에 추가
+
+        return redirect(post.get_absolute_url())
+
+    return render(request, "blog/create_new_post.html")
 
 
